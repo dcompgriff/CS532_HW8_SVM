4987c6d787d967b38c31c505014da7fdfafb6b78
Dan Griffin <dcompgriff@gmail.com>
2016-11-30 11:42:04 -0600

    Implemented gradient descent for regularized SVM, and saved plot of decision boundary.

--- HW8.py
+++ HW8.py
@@ -25,37 +25,119 @@ def q1():
     #row = (x1, x2, 1, label)
     reducedData = np.hstack((reducedData, np.ones((100, 1)), labels))
 
-    #Build LS classifier, and find w.
-    w = np.dot(np.linalg.pinv(reducedData[:, :-1]), reducedData[:, -1])
+    #q1_pa(reducedData)
+    q1_pb(reducedData)
 
-    #Generate decision boundary by passing in x from 0 to 8 at .1 spacing np.linspace(0, 8, 80)
-    #Classify as np.dot(x, w) > 0 = 1, or np.dot(x, w) < 0 = -1
+'''
+Regularized SVM training with gradient descent.
+'''
+def q1_pb(reducedData):
+
+    # #Initialize w0 to 0 vector.
+    # w = np.zeros((3, 1))
+    # #Initialize learning parameters.
+    # lambdaParam = 0.1
+    # gammaParam = 0.003
+    #
+    # for t in range(0, 20000):
+    #     if t % 100 == 0:
+    #         print('Iteration %d' % t)
+    #     grad = 0
+    #     errorSumTerm = 0
+    #     for i in range(0, reducedData.shape[0]):
+    #         #Calculate summed error term over training examples.
+    #         errorSumTerm += -reducedData[i, -1]*reducedData[i, :-1].reshape((reducedData.shape[1]-1, 1)) * (.5*(1 + np.sign(1 - (reducedData[i, -1]*np.dot(reducedData[i, :-1], w)))))[0]
+    #     #Calculate regularization term.
+    #     wErrorTerm = 2*lambdaParam*w
+    #     wErrorTerm[-1, 0] = 0
+    #     #Calculate gradient.
+    #     grad = errorSumTerm + wErrorTerm
+    #     #Update w term.
+    #     w = w - (gammaParam*grad)
+
+    w = trainSVM(reducedData)
+
+    #Plot SVM line.
     xList = np.linspace(0, 8, 80)
     yList = []
     for x in xList:
-        #Given x, solve for y.
-        yvalue = (1.0/w[1])*(-w[2]-(x*w[0]))
+        # Given x, solve for y.
+        yvalue = (1.0 / w[1]) * (-w[2] - (x * w[0]))
         yList.append(yvalue)
 
-    #Build plot.
+    # Build plot.
     plt.ylabel('feature 4 (petal width)')
     plt.xlabel('feature 3 (petal length)')
     plt.ylim((0, 3.5))
     plt.xlim((2, 8))
-    #Plot data points.
-    plt.scatter(reducedData[:50, 0], reducedData[:50, 1], color ='r')
+    # Plot data points.
+    plt.scatter(reducedData[:50, 0], reducedData[:50, 1], color='r')
     plt.scatter(reducedData[50:, 0], reducedData[50:, 1], color='b')
-    #Plot decision boundary.
+    # Plot decision boundary.
     plt.plot(xList, yList, color='black')
 
-    #Plot legend.
+    # Plot legend.
     red_patch = mpatches.Patch(color='red', label='virginica')
     blue_patch = mpatches.Patch(color='blue', label='versicolor')
     plt.legend(handles=[red_patch, blue_patch], loc=2)
     plt.show()
 
+def trainSVM(reducedData):
+    # Initialize w0 to 0 vector.
+    w = np.zeros((3, 1))
+    # Initialize learning parameters.
+    lambdaParam = 0.1
+    gammaParam = 0.003
 
-    print(data)
+    for t in range(0, 20000):
+        if t % 100 == 0:
+            print('Iteration %d' % t)
+        grad = 0
+        errorSumTerm = 0
+        for i in range(0, reducedData.shape[0]):
+            # Calculate summed error term over training examples.
+            errorSumTerm += -reducedData[i, -1] * reducedData[i, :-1].reshape((reducedData.shape[1] - 1, 1)) * \
+                            (.5 * (1 + np.sign(1 - (reducedData[i, -1] * np.dot(reducedData[i, :-1], w)))))[0]
+        # Calculate regularization term.
+        wErrorTerm = 2 * lambdaParam * w
+        wErrorTerm[-1, 0] = 0
+        # Calculate gradient.
+        grad = errorSumTerm + wErrorTerm
+        # Update w term.
+        w = w - (gammaParam * grad)
+
+    return w
+
+def q1_pa(reducedData):
+    # Build LS classifier, and find w.
+    w = np.dot(np.linalg.pinv(reducedData[:, :-1]), reducedData[:, -1])
+
+    # Generate decision boundary by passing in x from 0 to 8 at .1 spacing np.linspace(0, 8, 80)
+    # Classify as np.dot(x, w) > 0 = 1, or np.dot(x, w) < 0 = -1
+    xList = np.linspace(0, 8, 80)
+    yList = []
+    for x in xList:
+        # Given x, solve for y.
+        yvalue = (1.0 / w[1]) * (-w[2] - (x * w[0]))
+        yList.append(yvalue)
+
+    # Build plot.
+    plt.ylabel('feature 4 (petal width)')
+    plt.xlabel('feature 3 (petal length)')
+    plt.ylim((0, 3.5))
+    plt.xlim((2, 8))
+    # Plot data points.
+    plt.scatter(reducedData[:50, 0], reducedData[:50, 1], color='r')
+    plt.scatter(reducedData[50:, 0], reducedData[50:, 1], color='b')
+    # Plot decision boundary.
+    plt.plot(xList, yList, color='black')
+
+    # Plot legend.
+    red_patch = mpatches.Patch(color='red', label='virginica')
+    blue_patch = mpatches.Patch(color='blue', label='versicolor')
+    plt.legend(handles=[red_patch, blue_patch], loc=2)
+    plt.show()
+
 
 
 
